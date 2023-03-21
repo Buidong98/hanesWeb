6d7127ac03656cc451c0db893d4d0b211bc24c58
DESKTOP-RO0OOFR\admin <bndhy98@gmail.com>
2023-03-21 11:14:48 +0700

    update 21/03/23

--- routes/warehouse.routes.js
+++ routes/warehouse.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = new express.Router();
 const home = require("../controllers/warehouse/warehouseHome.controller.js")
-const warehouseController = require("../controllers/warehouse/shippingMarkScan.controller.js");
+const accountant = require("../controllers/warehouse/accountant.controller.js")
+const shippingMarkScan = require("../controllers/warehouse/shippingMarkScan.controller.js");
 const dashboard = require("../controllers/warehouse/dashboard.controller.js");
 router.get("/", home.getIndex);
-router.get("/shippingMarkScan",warehouseController.getIndex);
+router.get("/shippingMarkScan",shippingMarkScan.getIndex);
+router.get("/accountant",accountant.getIndex);
 router.get("/dashboard", dashboard.getIndex);
-router.post("/scanBarcode/poUpdate",warehouseController.poUpdate);
-
-router.post("/scanBarcode/CheckId",warehouseController.CheckId);
+router.post("/scanBarcode/poUpdate",shippingMarkScan.poUpdate);
+router.post("/scanBarcode/CheckId",shippingMarkScan.CheckId);
 module.exports = router;
